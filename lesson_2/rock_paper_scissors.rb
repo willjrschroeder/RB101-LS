81bb16c2909b3316bc8d94624921ae146e6efc75
Will Schroeder <willjrschroeder@gmail.com>
2021-12-31 10:43:45 -0600

    Updated game to Rock Paper Scissors Lizard Spock - added win counters

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,19 +1,31 @@
-VALID_CHOICES = ["rock", "paper", "scissors"]
+VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
+WIN_CONDITIONS = {"rock" => ["scissors", "lizard"], 
+                  "paper" => ["rock", "spock"],
+                  "scissors" => ["paper", "lizard"],
+                  "lizard" => ["spock", "paper"],
+                  "spock" => ["rock", "scissors"]}
+player_win_total = 0
+computer_win_total = 0
 
-def display_results(player, computer)
-  if win?(player, computer)
+def display_results(winner, player_win_total, computer_win_total)
+  if winner == "player"
     prompt "You won!"
-  elsif win?(computer, player)
+    prompt "Player total wins: #{player_win_total}"
+    prompt "Computer total wins: #{computer_win_total}"
+  elsif winner == "computer"
     prompt "Computer won!"
+    prompt "Player total wins: #{player_win_total}"
+    prompt "Computer total wins: #{computer_win_total}"
   else
     prompt "Tie game!"
+    prompt "Player total wins: #{player_win_total}"
+    prompt "Computer total wins: #{computer_win_total}"
   end
 end
 
 def win?(first, second)
-  (first == "rock" && second == "scissors") ||
-    (first == "paper" && second == "rock") ||
-    (first == "scissors" && second == "paper")
+  return true if WIN_CONDITIONS[first].include?(second)
+  false
 end
 
 def prompt(message)
@@ -26,6 +38,20 @@ loop do
     prompt "Choose one: #{VALID_CHOICES.join(', ')}"
     choice = gets.chomp
 
+    if choice.size == 1
+      VALID_CHOICES.each do |word|
+        if word =~ /#{choice}/
+          choice = word
+        end
+      end
+    elsif choice.size == 2
+      VALID_CHOICES.each do |word|
+        if word =~ /#{choice}/
+          choice = word
+        end
+      end
+    end
+
     break if VALID_CHOICES.include?(choice)
     prompt "Invalid choice! Please try again."
   end
@@ -33,7 +59,24 @@ loop do
   computer_choice = VALID_CHOICES.sample
 
   prompt "You chose #{choice}; Computer chose: #{computer_choice}."
-  display_results(choice, computer_choice)
+  
+  if win?(choice, computer_choice)
+    player_win_total += 1
+    display_results("player", player_win_total, computer_win_total)
+  elsif win?(computer_choice, choice)
+    computer_win_total += 1
+    display_results("computer", player_win_total, computer_win_total)
+  else
+    display_results("tie", player_win_total, computer_win_total)
+  end
+
+  if player_win_total == 3
+    prompt "You are the grand winner!"
+    break
+  elsif computer_win_total == 3
+    prompt "The computer is the grand winner!"
+    break
+  end
 
   prompt "Do you want to play again?"
   answer = gets.chomp
