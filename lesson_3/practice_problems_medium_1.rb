a4e6329e9329f8ec5ee557b7138c3125cd36beb6
Will Schroeder <willjrschroeder@gmail.com>
2022-01-03 13:28:09 -0600

    completed exercises

--- lesson_3/practice_problems_medium_1.rb
+++ lesson_3/practice_problems_medium_1.rb
@@ -6,3 +6,14 @@ end
 
 puts "the value of 40 + 2 is " + (40 + 2).to_s
 puts "the value of 40 + 2 is #{40 + 2}"
+
+def factors(number)
+  divisor = number
+  factors = []
+  loop do
+    break if divisor <= 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
