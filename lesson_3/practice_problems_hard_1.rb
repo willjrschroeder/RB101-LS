a747c1b61cf40035cd8c60a7e99e9c9a40887719
Will Schroeder <willjrschroeder@gmail.com>
2022-01-06 11:52:34 -0600

    completed exercises

--- lesson_3/practice_problems_hard_1.rb
+++ lesson_3/practice_problems_hard_1.rb
@@ -26,3 +26,13 @@ mess_with_vars(one, two, three)
 puts "one is: #{one}"
 puts "two is: #{two}"
 puts "three is: #{three}"
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false if dot_separated_words.size != 4
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  return true
+end
